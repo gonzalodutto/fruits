fdde36984b35881eb751b437a10670e3172febfc
gonzalodutto <gonzalodutto@gmail.com>
2022-07-04 16:38:15 +0200

    props done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import Title from "./components/Title";
 import FruitCard from "./components/FruitCard";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      {/* <main>
+        <Title content="Some Simple Title via <<Props>>" />
+      </main> */}
       <FruitCard
-        image="https://coding.images/react.jpg"
-        title="What is React all about?"
+        image="https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg"
+        name="Banana"
         description="React is all about one-way data flow, the Virtual DOM, and transpiling JSX."
       />
       <FruitCard
-        image="https://animal.images/kid.jpg"
-        title="A lovely kid"
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Kiwi_%28Actinidia_chinensis%29_1_Luc_Viatour.jpg/800px-Kiwi_%28Actinidia_chinensis%29_1_Luc_Viatour.jpg"
+        name="Kiwi"
         description="In fact, a kid is also the name of a baby goat!"
       />
       <FruitCard
-        image="https://another.nonexistent/image.jpg"
-        title="On placeholder image URLs"
+        image="https://leprimeurduchef.com/260-large_default/fruit-of-passion.jpg"
+        name="Passion Fruit"
         description="So yeah, you won't be able to look these images up. They're placeholders"
       />
     </div>
--- src/components/FruitCard.js
+++ src/components/FruitCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function FruitCard() {
-  return <div>FruitCard</div>;
+export default function FruitCard(props) {
+  return (
+    <div>
+      <h1>{props.name} </h1>
+      <img src={props.image} alt={props.name} width="150" />
+      <p>{props.description} </p>
+    </div>
+  );
 }
